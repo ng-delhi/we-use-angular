4575049b8d9b4029044152dfad6adee2d36d3a1c
Rohtash Sethi <rohtash.sethi@gmail.com>
2025-05-30 10:50:28 +0530

    chore(icons): use remix icons for theme icons

--- src/app/core/components/header.ts
+++ src/app/core/components/header.ts
@@ -15,7 +15,7 @@ import { ThemeService } from '../services/theme';
       </div>
 
       <button (click)="themeService.toggleTheme()" class="theme-btn">
-        <i class="fa-solid {{ themeService.isDarkMode() ? 'fa-moon' : 'fa-sun' }}"></i>
+        <i class="{{ themeService.isDarkMode() ? 'ri-moon-fill' : 'ri-sun-fill' }}"></i>
       </button>
     </header>
   `,
